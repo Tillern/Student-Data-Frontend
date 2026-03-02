2687a32d32275cd9aff378c328c719ebb601a3e1
Tillern <brysonminodi@gmail.com>
2026-03-02 15:29:30 +0300

    created a feature based architecture with all modules and compenents need for start

---
+++ src/app/features/data-generation/generate/generate.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generate',
+  imports: [],
+  templateUrl: './generate.html',
+  styleUrl: './generate.scss',
+})
+export class Generate {}
