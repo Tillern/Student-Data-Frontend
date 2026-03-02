74015083ef311cc2de33ba76bcdce049545dc4ea
Tillern <brysonminodi@gmail.com>
2026-03-02 15:22:37 +0300

    initial project

---
+++ src/app/features/data-generation/data-generation-module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DataGenerationRoutingModule } from './data-generation-routing-module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, DataGenerationRoutingModule],
+})
+export class DataGenerationModule {}
---
+++ src/app/features/data-processing/data-processing-module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DataProcessingRoutingModule } from './data-processing-routing-module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, DataProcessingRoutingModule],
+})
+export class DataProcessingModule {}
